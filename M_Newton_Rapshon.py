d1818ee95b900b3172d9cfd360637c7d22fd43da
Aldo Yael Guzmán Ramírez <yaelguz27@gmail.com>
2020-10-25 17:00:08 -0600

    Quite arreglos de prueba

--- M_Newton_Rapshon.py
+++ M_Newton_Rapshon.py
@@ -6,6 +6,7 @@ x=sy.symbols('x')
 funcion=((1/2)*sin(x))-x+2
 #primera derivada de la funcion
 onederivada=sy.diff(funcion,x)
+#Segunda derivada
 twoderivada=sy.diff(onederivada, x)
 #Valor en el cual converge la funcion a+b/2
 x_0=2.5
@@ -13,7 +14,9 @@ xr=x_0
 
 #se subtituye x por xr a continuandicon se evalua
 fx=funcion.evalf(subs={x: xr})
+#Primera derivada valuada numericamente
 oneD=onederivada.evalf(subs={x: xr})
+#segunda derivada valuada numericamente
 twoD=twoderivada.evalf(subs={x: xr})
 #Metodo de convergencia
 X0=abs((fx*twoD)/(oneD**2))
@@ -22,9 +25,7 @@ X0=abs((fx*twoD)/(oneD**2))
 Eabs=100/100
 #Dando valor al contador
 contador=0
-tabla=[]
-tabla1=[]
-tabla2=[]
+
 print("i\t|xi\t\t\t|eabs|")
 #Verificando que el valor absoluto sea menor a cerp
 while Eabs>0:
