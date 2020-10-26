70f4e21d20175bd46daea47f13bb5630353ea1c6
Aldo Yael Guzmán Ramírez <yaelguz27@gmail.com>
2020-10-25 18:51:32 -0600

    Programa terminado

--- M_Newton_Rapshon.py
+++ M_Newton_Rapshon.py
@@ -2,43 +2,58 @@ from sympy import sin
 import sympy as sy
 #x es un simbolo
 x=sy.symbols('x')
-#funcion 
+#funcion original
 funcion=((1/2)*sin(x))-x+2
 #primera derivada de la funcion
 onederivada=sy.diff(funcion,x)
-#Segunda derivada
+#Segunda derivada de la funcion
 twoderivada=sy.diff(onederivada, x)
-#Valor en el cual converge la funcion a+b/2
+#Intervalo que converge a+b/2 donde a= 2 y b=3
 x_0=2.5
+#Le damos valor a xr
 xr=x_0
 
 #se subtituye x por xr a continuandicon se evalua
 fx=funcion.evalf(subs={x: xr})
-#Primera derivada valuada numericamente
+#primera derivada  numerica
 oneD=onederivada.evalf(subs={x: xr})
-#segunda derivada valuada numericamente
+#Segunda derivada numerica
 twoD=twoderivada.evalf(subs={x: xr})
 #Metodo de convergencia
-X0=abs((fx*twoD)/(oneD**2))
-#print("Valor de la convergencia: ",fx,"\n")
-#iteraciones
+X0=abs((fx*twoD)/(oneD*oneD))
+#Solo es un salto de linea
+print("")
+#Nos muestra la funcion
+print("La funcion es: ", funcion)
+#Muestra La primera derivada
+print("La primera derivada es: ", onederivada)
+#Muestra la segunda derivada
+print("La segunda derivada es: ", twoderivada)
+#Muestra valor de convergencia X0<1 para que converga
+print("Valor de la convergencia: ",X0,"\n")
+#Asignamos valores iniciales solo para que el programa no marque error
 Eabs=100/100
-#Dando valor al contador
 contador=0
-
-print("i\t|xi\t\t\t|eabs|")
-#Verificando que el valor absoluto sea menor a cerp
+#Datos de la tabla
+print("--------------------------------------------------------------------------------------------------------------------")
+print("| i  |\t\t\txi\t\t\t|\t\t\tf(x)\t\t\t|\t\tf'(x)\t\t\t|\teabs")
+print("--------------------------------------------------------------------------------------------------------------------")
+#Iniciamos con las iteraciones
 while Eabs>0:
-    #Xi-1 es decir el anterior
+#Nuevo valor de xi en otras palabras x(i+1)
     xa=xr
-    #Numero de iteraciones
-    contador+=1
-    #Metodo de Newton Rapshon
-    M_Newton_R=x-(funcion/onederivada)
-    #Numero valor de Xi
-    xr=M_Newton_R.evalf(subs={x: xr})
-    #Nueva funcion valuada
+#Nueva funcion numerica
     nuevafx=funcion.evalf(subs={x: xr})
-    #Error absoluto
+#Nueva primera derivada numerica
+    nuevafxuno=onederivada.evalf(subs={x: xr})
+#Aplicando Metodo de Newton Rapshon 
+    M_Newton_R=x-(funcion/onederivada)
+#Asignando nuevo valor de xi
+    xr=M_Newton_R.evalf(subs={x: xr})
+#Obteniendo el error absoluto
     Eabs=abs(xr-xa)
-print("Iteracion: ",contador,"\t|","Raiz: ",xa,"\t|","Error: ",Eabs,"|")
+#Impresion de los datos obtenidos
+    print("| ",contador,"|","{:.14f}".format(xa),"\t|","{:.20f}".format(nuevafx),"\t|","{:.14f}".format(nuevafxuno),"\t|",Eabs)
+    print("--------------------------------------------------------------------------------------------------------------------")
+#Controlador de iteraciones
+    contador+=1
